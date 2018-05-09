a65008a87047231c2ea8b68a7da3931d809326c0
javierjcf <javierjcf@gmail.com>
2018-05-09 10:10:30 +0200

    [MIG]02b_stock_filter

---
+++ project-addons/o2b_stock_filter/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "O2B Create Filter for stock invoice Main Company(A)",
+    "version": "1.9.1",
+    "author": "O2B Technologies",
+    'website': 'http://www.o2b.co.in',
+    "depends": ["sale", "stock", "base"],
+    "data": [
+        'views/stock_view.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+}
