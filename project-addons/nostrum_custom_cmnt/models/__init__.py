e5d0c1b5092663264371795ba4531f804a92592c
javierjcf <javierjcf@gmail.com>
2018-05-21 17:07:34 +0200

    [ADD]nostrum_custom_cmnt: El albarán no es valorado por defecto

--- project-addons/nostrum_custom_cmnt/models/__init__.py
+++ project-addons/nostrum_custom_cmnt/models/__init__.py
@@ -2,6 +2,7 @@
 # © 2018 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import partner
 from . import invoice
 from . import sale
 from . import stock
