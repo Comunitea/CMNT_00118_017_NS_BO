48b45134f856d5ef899eafa7554f5fb97edca0a9
javierjcf <javierjcf@gmail.com>
2018-06-28 17:54:20 +0200

    [IMP]nostrum_custom_cmnt: Teléfono a la factura, coger precio de coste si no hay proveedor

--- post-migration-after-update.sql
+++ post-migration-after-update.sql
@@ -5,10 +5,11 @@
 update product_template set internal_note = description_sale;
 update product_template set description_sale=null;
 
--- Copiar descripción del nombre y de los plazos de pago
+-- Copiar descripción del nombre y de los plazos de pago. Hacerlo despues de duplicar
 update account_payment_term set note=name;
 update account_payment_mode set note=name;
 
 -- Albarán valorado por defecto false
 update res_partner set valued_picking = False;
 
+UPDATE ir_module_module SET state = 'to remove' WHERE name = 'nan_partner_risk';
--- project-addons/nostrum_custom_cmnt/models/purchase.py
+++ project-addons/nostrum_custom_cmnt/models/purchase.py
@@ -8,3 +8,22 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     payment_term_id = fields.Many2one(required=True, readonly=False)
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    def _onchange_quantity(self):
+        res = super(PurchaseOrderLine, self)._onchange_quantity()
+        seller = self.product_id._select_seller(
+            partner_id=self.partner_id,
+            quantity=self.product_qty,
+            date=self.order_id.date_order and self.order_id.date_order[:10],
+            uom_id=self.product_uom)
+        # Set cost to purchase price if not seller and price unit defined.
+        if not seller and self.product_id and not self.price_unit:
+            cost = self.product_id.standard_price
+            price_unit = self.product_id.uom_po_id.\
+                _compute_price(cost, self.product_id.uom_po_id)
+            self.price_unit = price_unit
+        return res
