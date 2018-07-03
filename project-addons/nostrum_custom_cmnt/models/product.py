f701622465fb5d09846a11fb3cecad83837a643a
javierjcf <javierjcf@gmail.com>
2018-07-03 14:33:13 +0200

    [ADD]nostrum_custom_cmnt: Añadido código en el nombre de las variantes de los elementos de tarifa

--- project-addons/nostrum_custom_cmnt/models/product.py
+++ project-addons/nostrum_custom_cmnt/models/product.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 # © 2018 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     internal_note = fields.Text('Internal Note')
+
+
+class PricelistItem(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    @api.one
+    def _get_pricelist_item_name_price(self):
+        res = super(PricelistItem, self)._get_pricelist_item_name_price()
+        # Show alwais code
+        if self.product_id:
+            self.name = self.product_id.display_name
+        return res
