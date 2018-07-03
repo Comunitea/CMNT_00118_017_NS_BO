c750fc06c3e5d8d0c2840f631b7a295ea6db54dc
javierjcf <javierjcf@gmail.com>
2018-07-03 12:58:32 +0200

    [FIX]nostrum:_custom_cmnt: No modificar descuento al cambiar qty en ventas, subtotal añadido a listado facturas

--- project-addons/nostrum_custom_cmnt/models/sale.py
+++ project-addons/nostrum_custom_cmnt/models/sale.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2018 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class SaleOrder(models.Model):
@@ -17,3 +17,43 @@ class SaleOrderLine(models.Model):
     partner_id = fields.Many2one('res.partner', 'Partner',
                                  related='order_id.partner_id')
     date_order = fields.Datetime('Date Order', related='order_id.date_order')
+
+    # FULL OVERWRITED TO AVOID SET DISCOUNT = 0 WHEN CHANGING QRT
+    @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty',
+                  'tax_id')
+    def _onchange_discount(self):
+        # self.discount = 0.0  COMMENTED
+        if not (self.product_id and self.product_uom and
+                self.order_id.partner_id and self.order_id.pricelist_id and
+                self.order_id.pricelist_id.
+                discount_policy == 'without_discount' and
+                self.env.user.has_group('sale.group_discount_per_so_line')):
+            return
+
+        context_partner = dict(self.env.context,
+                               partner_id=self.order_id.partner_id.id,
+                               date=self.order_id.date_order)
+        pricelist_context = dict(context_partner, uom=self.product_uom.id)
+
+        price, rule_id = self.order_id.pricelist_id.\
+            with_context(pricelist_context).\
+            get_product_price_rule(self.product_id,
+                                   self.product_uom_qty or 1.0,
+                                   self.order_id.partner_id)
+        new_list_price, currency_id = self.with_context(context_partner).\
+            _get_real_price_currency(self.product_id, rule_id,
+                                     self.product_uom_qty,
+                                     self.product_uom,
+                                     self.order_id.pricelist_id.id)
+
+        if new_list_price != 0:
+            if self.order_id.pricelist_id.currency_id.id != currency_id:
+                # we need new_list_price in the same currency as price,
+                # which is in the SO's pricelist's currency
+                new_list_price = self.env['res.currency'].\
+                    browse(currency_id).with_context(context_partner).\
+                    compute(new_list_price,
+                            self.order_id.pricelist_id.currency_id)
+            discount = (new_list_price - price) / new_list_price * 100
+            if discount > 0:
+                self.discount = discount
