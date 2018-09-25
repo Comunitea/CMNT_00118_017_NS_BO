4101b4091e828b7e6a0664a1d588a453f1a0c43f
Tu Nombre <pavel@comunitea.com>
2018-09-25 11:27:44 +0200

    [UPD] theme_adds

---
+++ project-addons/theme_adds/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2018 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'Tema addicional para Clarico Theme',
+    'version': '1.0',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'depends': ['website_sale'],
+    'data': [
+        'views/menu.xml',
+        'views/shop_home.xml',
+        'views/contactus.xml',
+        'views/footer.xml',
+        'views/header.xml',
+        'views/views.xml',
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+}
