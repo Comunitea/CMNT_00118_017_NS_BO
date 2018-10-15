a1758601dde437d304bb04f08c41e9368c4f5e35
neo-oien <i@oien.ru>
2018-10-15 14:37:28 +0200

    [UPD] Start page, Category bread crumbs

--- project-addons/theme_adds/static/js/script.js
+++ project-addons/theme_adds/static/js/script.js
@@ -1,4 +1,27 @@
+/* Category menu toggle open */
 $('a.to-toggle-menu').click(function(){
     $('.custom-menu-inside-div').toggle();
     return false;
+});
+/* Breadcrumb in category page */
+$(document).ready(function(){
+    //alert(document.location.pathname + document.location.search);
+});
+$( ".all-category-div li a" ).each(function() {
+    var url = document.location.pathname + document.location.search;
+    var current= $(this);
+    if (current.attr("href") == url ){
+        $(".select-nevigation-span").html("/");
+        $(".select-nevigation-child").attr("href", url);
+        $(".select-nevigation-child").html(current.html());
+    }
+});
+$( ".wp_products_grid_tags a.product-tag" ).each(function() {
+    var url = document.location.pathname + document.location.search;
+    var current= $(this);
+    if (current.attr("href") == url ){
+        $(".select-nevigation-span").html("/");
+        $(".select-nevigation-child").attr("href", url);
+        $(".select-nevigation-child").html(current.html());
+    }
 });
