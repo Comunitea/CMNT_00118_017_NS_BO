f087aff68ceae53f8a1aa9a07896820c6497bf70
neo-oien <i@oien.ru>
2018-11-05 12:43:44 +0100

    [ADD] Ajax add to cart

--- project-addons/theme_adds/static/js/script.js
+++ project-addons/theme_adds/static/js/script.js
@@ -48,3 +48,75 @@ $( ".wp_products_grid_tags a.product-tag" ).each(function() {
         $(".select-nevigation-child").html(current.html());
     }
 });
+
+/* Ajax "Add to cart" */
+odoo.define('product_quick_view.quick_view', function (require) {
+    "use strict";
+
+    var ajax = require('web.ajax');
+    var msg_success = 'This product was added to cart';
+    //
+    // On the shop page
+    //
+    $('.oe_product_cart form').on('submit', function(event){
+        event.preventDefault();
+
+        var msg_div = $(this).find('.add-to-cart-message');
+        var prod_id = $(this).find('input[name="product_id"]').val();
+
+        ajax.jsonRpc("/shop/cart/update_json", 'call', {
+            'product_id': parseInt(prod_id, 10),
+            'set_qty': 1
+        }).then(function (data) {
+            if (data.warning) {
+                $(msg_div).html(data.warning);
+                $(msg_div).parent().addClass('add-error').slideDown();
+            } else {
+                $(msg_div).html(msg_success);
+                $(msg_div).parent().removeClass('add-error').slideDown();
+            }
+            // cart quantity change
+            $('#header-cart .my_cart_quantity').html(data.cart_quantity);
+            // auto close
+            setTimeout(function(){
+                $(msg_div).parent().slideUp();
+            }, 4500);
+        });
+    });
+    //
+    // On the product page
+    //
+    $('.p_ad2cart a#add_to_cart').each(function(){
+        $(this).removeAttr('href'); // disable page scroll to top
+    });
+    $('form.js_add_cart_variants').on('submit', function(event){
+        event.preventDefault();
+
+        var msg_div  = $(this).find('.add-to-cart-message');
+        var prod_id  = $(this).find('input[name="product_id"]').val();
+        var prod_qty = $(this).find('input[name="add_qty"]').val();
+
+        ajax.jsonRpc("/shop/cart/update_json", 'call', {
+            'product_id': parseInt(prod_id, 10),
+            'set_qty': parseInt(prod_qty, 10)
+        }).then(function (data) {
+            if (data.warning) {
+                $(msg_div).html(data.warning);
+                $(msg_div).parent().addClass('add-error').slideDown();
+            } else {
+                $(msg_div).html(msg_success);
+                $(msg_div).parent().removeClass('add-error').slideDown();
+            }
+            // cart quantity change
+            $('#header-cart .my_cart_quantity').html(data.cart_quantity);
+            // auto close
+            setTimeout(function(){
+                $(msg_div).parent().slideUp();
+            }, 4500);
+        });
+    });
+});
+/* Close popup "Was added" message */
+$('.wp-close').click(function(){
+    $(this).parent().slideUp();
+});
