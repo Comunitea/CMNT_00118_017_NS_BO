107d335c98564abb8c6dad1b183be2adc4833588
ruben <ruben@comunitea.com>
2018-11-29 17:08:13 +0100

    [ADD] Dependencia modulo payment_acquirer_by_amount

--- project-addons/theme_adds/__manifest__.py
+++ project-addons/theme_adds/__manifest__.py
@@ -2,19 +2,48 @@
 # © 2018 Comunitea
 # Ruben Seijas <ruben@comunitea.com> - Pavel Smirnov <pavel@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#
+##############################################################################
+#
+#    Copyright (C) {year} {company} All Rights Reserved
+#    ${developer} <{mail}>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
     'name': 'Licenses: Theme Adds Clarico',
     'version': '1.0',
     'summary': 'Tema addicional para Clarico Theme',
+    'description': 'Modifica y adapta el tema Clarico Theme a las necesidades del sitio web',
     'author': 'Comunitea',
+    'website': 'http://www.comunitea.com',
     'license': 'AGPL-3',
     'category': 'Custom',
+    'contributors': [
+        "Comunitea ",
+        "Ruben Seijas <ruben@comunitea.com>",
+        "Pavel Smirnov <pavel@comunitea.com>"
+    ],
     'depends': [
         'website_sale',
         'website_cookie_notice',
         'website_product_tags_73lines',
         'website_crm_recaptcha',
         'website_mail_channel',
+        'payment_acquirer_by_amount',
         'payment_redsys',
         'payment_paypal',
         'website_sale_one_step_checkout_delivery',
@@ -42,9 +71,8 @@
         'views/terms.xml',
         'views/category_view.xml',
     ],
-    'images': [
-        'static/description/icon.png',
+    'qweb': [
     ],
-    'website': 'http://www.comunitea.com',
     'installable': True,
+    'application': True,
 }
